b817b971b62504ed0357b5bdea26d8c83f4c811a
Juan Pico <japm1420@gmail.com>
2025-07-18 01:12:51 -0500

    Playing with my first views

--- ContentView.swift
+++ ContentView.swift
@@ -8,19 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    let juan = "Juan"
     var body: some View {
-        VStack {	Button("Mi primer Boton") {
+            Text("probando SwiftUI \(juan)")
+            ContentView2()
+        }
+        
+        
+}
+
+
+struct ContentView2: View {
+    var body: some View {
+        let juan2 = "Juan2"
+        VStack {    Button("Mi primer Boton") {
             /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
         }
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Aplicacion creado por \(juan2)")
         }
         .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct contentViewPreviewL: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
